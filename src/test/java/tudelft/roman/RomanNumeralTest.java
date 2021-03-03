16c2b666f5357b8c1fb921cacfec165ff40e2126
Despoina Karavia <despoina.karavia@leica-geosystems.com>
2021-03-03 18:00:35 +0100

    Run unit tests on MyRomanNumber class

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -8,28 +8,28 @@ public class RomanNumeralTest {
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
+        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
+        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
+        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
+        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
