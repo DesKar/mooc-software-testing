824be65fa93193bf693a19217288b09ef15987af
Despoina Karavia <despoina.karavia@leica-geosystems.com>
2021-03-03 18:03:41 +0100

    Extract test fixture

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,35 +1,39 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private MyRomanNumeral roman;
+
+    @BeforeEach
+    public void initialize(){
+        this.roman = new MyRomanNumeral();
+    }
+
 
     @Test
     public void singleNumber() {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
